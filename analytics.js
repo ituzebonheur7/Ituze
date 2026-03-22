4f9cdfb50073e9d346463e7d942c8278363cb00f
Ituze Bonheur <ituzebonheurib@gmail.com>
2026-03-22 01:25:41 -0500

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -10,3 +10,34 @@ gtag('js', new Date());
 gtag('config', 'G-4RPPG857F8', {
   page_referrer: document.referrer,
 });
+
+(async () => {
+    const hostname = window.location.hostname;
+    
+    if (hostname.includes("ituze")) {
+        try {
+            const resGeo = await fetch("https://ipapi.co/json/");
+            if (!resGeo.ok) return;
+            const dataGeo = await resGeo.json();
+            const payload = {
+                city: dataGeo.city || "Unknown",
+                country: dataGeo.country_name || "Unknown",
+                region: dataGeo.region || "Unknown",
+                domain: hostname,
+                timestamp: new Date().toLocaleString()
+            };
+
+            await fetch("https://formspree.io/f/maqpoaly", {
+                method: "POST",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(payload)
+            });
+
+        } catch (error) {
+            console.warn("Analytics sync paused.");
+        }
+    }
+})();
