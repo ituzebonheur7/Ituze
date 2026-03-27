7bed196a30b5088437da3fc29dd20fd4431ed503
Ituze Bonheur <ituzebonheurib@gmail.com>
2026-03-27 17:45:34 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,47 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/7.4.0/workbox-sw.js');
 
-const OFFLINE_URL = '/offline.html';
+const OFFLINE_HTML = '/offline.html';
+const FALLBACK_IMAGE = '/images/ituze.png'; 
 
 workbox.precaching.precacheAndRoute([
-  { url: OFFLINE_URL, revision: null }
+  { url: OFFLINE_HTML, revision: '1.0.1' },
+  { url: FALLBACK_IMAGE, revision: '1.0.0' }
 ]);
 
 workbox.routing.registerRoute(
-  ({ request }) =>
-    request.destination === 'style' ||
-    request.destination === 'script',
-  new workbox.strategies.StaleWhileRevalidate()
+  ({ request }) => 
+    request.destination === 'style' || 
+    request.destination === 'script' || 
+    request.destination === 'worker',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'assets-cache',
+  })
 );
 
 workbox.routing.registerRoute(
+  ({ request }) => request.destination === 'image',
+  new workbox.strategies.CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 50,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
+
+const htmlHandler = new workbox.strategies.NetworkFirst({
+  cacheName: 'html-cache',
+});
+
+workbox.routing.registerRoute(
   ({ request }) => request.destination === 'document',
-  async ({ event }) => {
-
-    if (!self.navigator.onLine) {
-      return caches.match(OFFLINE_URL);
-    }
-
-    // Otherwise try network
+  async (args) => {
     try {
-      return await fetch(event.request);
+      return await htmlHandler.handle(args);
     } catch (error) {
-      return caches.match(OFFLINE_URL);
+      return caches.match(OFFLINE_HTML);
     }
   }
 );
